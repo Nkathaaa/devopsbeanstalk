7f2c0a25077e421bf8adb246627e0512a7a43f8d
Nkathaaa <catherinesheilankatha@gmail.com>
2024-05-08 16:43:03 +0300

    added 11

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> The is Sheila Nkatha
+          Edit <code>src/App.js</code> The is yule Sheila Nkatha
         </p>
         <a
           className="App-link"
