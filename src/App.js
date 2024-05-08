98edbd8ed10863c7123cbf9a93827d0da8fe807c
Nkathaaa <catherinesheilankatha@gmail.com>
2024-05-08 17:06:59 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> The is yule Sheila Nkatha
+          Edit <code>src/App.js</code> and save to reload.Wanjiku
         </p>
         <a
           className="App-link"
